c6ea393ede59b3138351893d3917325c829691f1
charlie <a53x@shitposting.expert>
2025-02-18 20:13:30 +0200

    Update reader.svelte

--- src/routes/blog/entry/reader.svelte
+++ src/routes/blog/entry/reader.svelte
@@ -14,19 +14,19 @@
 </script>
 
 <svelte:head>
-	<title>{title} | allissa's blog</title>
-	<meta name="description" content="allissa's comfy blog" />
-	<meta property="og:title" content="{title} | allissa's blog" />
-	<meta property="og:description" content="allissa's comfy blog" />
+	<title>{title} | charlie's blog</title>
+	<meta name="description" content="charlie's comfy blog" />
+	<meta property="og:title" content="{title} | charlie's blog" />
+	<meta property="og:description" content="charlie's comfy blog" />
 </svelte:head>
 
-<p><a href="https://itzzen.net/blog"><i class="fa-solid fa-arrow-left"></i> back to my blog</a></p>
+<p><a href="https://t480.dev/blog"><i class="fa-solid fa-arrow-left"></i> back to my blog</a></p>
 
 <h2>{title}</h2>
 <p>publish date: {date}</p>
 {#if modified}
 	<p>modified date: {modified}</p>
 {/if}
-<p>author: itzzen@tutamail.com</p>
+<p>author: charlie (a53x@shitposting.expert)</p>
 <hr />
 <slot />
