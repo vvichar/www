fb4cd01750161eb689003f6a60efd5b11f6cce1a
humanoid Apollo <txfnc@dmc.chat>
2025-03-26 20:57:27 +0200

    updating stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,6 +29,7 @@
 			<a href="/blog/" class:active={$page.url.pathname == '/blog/'}><i class="fa-solid fa-newspaper"></i> my blog</a>
 			<a href="/about" class:active={$page.url.pathname == '/about/'}><i class="fa-solid fa-address-card"></i> about me</a>
 			<a href="/projects" class:active={$page.url.pathname == '/projects/'}><i class="fa-solid fa-code"></i> projects</a>
+			<a href="/resources" class:active={$page.url.pathname == '/resources/'}><i class="fa-solid fa-folder"></i> resources</a>
 			<a href="/contact" class:active={$page.url.pathname == '/contact/'}><i class="fa-solid fa-paper-plane"></i> contact</a>
 			<a href="/presence" class:active={$page.url.pathname == '/presence/'}><i class="fa-solid fa-user"></i> presence</a>
 			<a href="/donate" class:active={$page.url.pathname == '/donate/'}><i class="fa-solid fa-piggy-bank"></i> donate</a>
@@ -47,6 +48,6 @@
 </main>
 
 <footer>
-	<p><i>made with</i> ❤ <i>by charlie | <i>site last modified Feburary 18th 2025</i> | <a href="https://github.com/a53xd/www">source code viewable here, credits to itzzen for code</a></i></p>
+	<p><i>made with</i> ❤ <i>by charlie | <i>site last modified March 26th 2025</i> | <a href="https://github.com/a53xd/www">source code viewable here, credits to itzzen for code</a></i></p>
 	<p><img src="{blahaj}" style="height:15px;"> certified Good Website by blåhaj</p>
 </footer>
---
+++ src/routes/resources/+page.md
@@ -0,0 +1,7 @@
+## Resources and other junk
+
+I needed a place on my website to dump some files, so welcome to my resource page for all files related to charlie and the site
+
+| file name  | description            | link                                           |
+| ---------- | ---------------------- | ---------------------------------------------- |
+| pubkey.txt | my OpenPGP public key. | [/resources/pubkey.gpg](/resources/pubkey.txt) |
